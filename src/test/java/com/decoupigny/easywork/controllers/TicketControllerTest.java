1490524b1839c60bd05faf7275630b99a1f3b9c6
Akazan83 <decoupigny.n@gmail.com>
2022-01-31 17:37:07 +0100

    circleci test

---
+++ src/test/java/com/decoupigny/easywork/controllers/TicketControllerTest.java
@@ -0,0 +1,36 @@
+package com.decoupigny.easywork.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TicketControllerTest {
+
+    @Test
+    void getAllTickets() {
+    }
+
+    @Test
+    void getTicketById() {
+    }
+
+    @Test
+    void createTicket() {
+    }
+
+    @Test
+    void updateTicket() {
+    }
+
+    @Test
+    void deleteTicket() {
+    }
+
+    @Test
+    void deleteAllTickets() {
+    }
+
+    @Test
+    void findByStatus() {
+    }
+}
